abb92c3fab33234009d2aaca893f73861dcc07b9
Andriy Chernikov <chernikov@gmail.com>
2024-11-11 12:17:33 +0200

    Add block ui

--- Calendary.Ng/src/app/app.config.ts
+++ Calendary.Ng/src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
-import { AuthInterceptor } from '../interceptors/auth.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import localeUk from '@angular/common/locales/uk';
 import { registerLocaleData } from '@angular/common';
+import { AuthInterceptor } from '../interceptors/auth.interceptor';
 import { ErrorInterceptor } from '../interceptors/error.interceptor';
+import { BlockUiInterceptor } from '../interceptors/block.ui.interceptor';
 
 registerLocaleData(localeUk);
 
@@ -26,6 +27,10 @@ export const appConfig: ApplicationConfig = {
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ErrorInterceptor,
+      multi: true
+    },
+    { provide: HTTP_INTERCEPTORS, 
+      useClass: BlockUiInterceptor,
       multi: true
     },
     { provide: LOCALE_ID, useValue: 'uk' },
--- Calendary.Ng/src/app/main.component.ts
+++ Calendary.Ng/src/app/main.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { BlockUiService } from '../services/block.ui.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
 })
 export class MainComponent {
+
+  isBlocked$: Observable<boolean>; 
+
+  constructor(private blockUiService : BlockUiService) {
+    this.isBlocked$ = this.blockUiService.isBlocked$;
+  } 
+ 
+  
 }
