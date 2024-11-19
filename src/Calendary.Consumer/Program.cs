ef198c5e3e5222f96bee7290a7cb16cb0dc5d612
Andriy Chernikov <chernikov@gmail.com>
2024-11-19 10:16:23 +0200

    rabbit mq WIP

---
+++ src/Calendary.Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Calendary.Repos;
+using Calendary.Consumer;
+using Microsoft.Extensions.Configuration;
+using Calendary.Consumer.Consumers;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        // Підключення до бази даних
+        var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+        services.AddCalendaryRepositories(connectionString);
+
+        // Реєстрація RabbitMQ Consumer
+        services.AddHostedService<JobTaskConsumer>();
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ src/Calendary.Model/Messages/CreatePredictionMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Calendary.Model.Messages;
+
+public record CreatePredictionMessage
+{
+    public int JobId { get; set; }
+    public int UserId { get; set; }
+    public List<int> TaskIds { get; set; } = new List<int>();
+}
